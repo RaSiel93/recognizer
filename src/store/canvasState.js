c4029cecfe9e3135e86a0f90f0af3be87301bb91
alex <aleksandrpoplawskiy@gmail.com>
2022-08-17 16:56:22 +0300

    config autodeploy from github

--- src/store/canvasState.js
+++ src/store/canvasState.js
@@ -92,14 +92,6 @@ class CanvasState {
 		this.tool = new Rectangle(canvas, this);
 		this.listen();
 	}
-
-  download = (content, fileName, contentType) => {
-    var a = document.createElement("a");
-    var file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-  }
 }
 
 export default new CanvasState;
